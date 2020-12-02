20e730c24522c4ccd7c4d15a6a88f052f007d1e5
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-02 10:42:54 +1000

    advent of code 2020 solutions

---
+++ d1p1-solution.py
@@ -0,0 +1,6 @@
+f = open('d1p1-input.txt', 'r')
+data = f.readlines()
+data = list(map(int,data))
+for x in data:
+	for y in data:
+		if x + y == 2020: print(x,y, x*y)
---
+++ d1p2-solution.py
@@ -0,0 +1,7 @@
+f = open('d1p1-input.txt', 'r')
+data = f.readlines()
+data = list(map(int,data))
+for x in data:
+	for y in data:
+		for z in data:
+			if x + y + z == 2020: print(x,y,z x*y*z)
