11557267803ef9b68e88b02c76ccf03788b824a9
vxoli <vxoli-dfoxb@yahoo.co.uk>
2020-12-03 22:24:29 +1000

    Update d3p2-solution.py

--- d3p2-solution.py
+++ d3p2-solution.py
@@ -7,7 +7,6 @@ for route in range(len(routes)):
     col = 0
     trees = 0
     for row in range(0, len(data), routes[route][1]):
-        print(route,row,col)
         if data[row][col] == "#":
                 trees += 1
         col = (col + routes[route][0]) % (length-1)
