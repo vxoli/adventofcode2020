094bfb56d4ae116815b90ebd44424bbb1c7ee108
vxoli <vxoli-dfoxb@yahoo.co.uk>
2020-12-03 22:15:47 +1000

    Update d3p2-solution.py
    add 0 to for loop: for row in range(0, len(data), routes[route][1]): so routes[route][1] becomes the step not the end of the range.

--- d3p2-solution.py
+++ d3p2-solution.py
@@ -6,7 +6,7 @@ routes = [(1,1),(3,1),(5,1),(7,1),(1,2)]
 for route in range(len(routes)):
     col = 0
     trees = 0
-    for row in range(len(data), routes[route][1]):
+    for row in range(0, len(data), routes[route][1]):
         print(route,row,col)
         if data[row][col] == "#":
                 trees += 1 
