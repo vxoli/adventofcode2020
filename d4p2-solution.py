6d9aa41b90dca7ad8aaf49ceecc2bd5711b5e436
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-05 10:28:34 +1000

    d4 problem 2

---
+++ d4p2-solution.py
@@ -0,0 +1,11 @@
+f = open('d4-input.txt', 'r')
+data = f.readlines()
+data_str = ""
+valid = 0
+for lines in data:
+	data_str += lines
+data_str = data_str.split("\n\n")
+for passport in data_str:
+	if ("byr" in passport) and ("iyr" in passport) and ("eyr" in passport) and ("hgt" in passport) and ("hcl" in passport) and ("ecl" in passport) and ("pid" in passport):
+		valid = valid + 1
+print(valid)
