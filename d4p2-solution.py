79e1e9b6a9b581b14bb1b461a814d01d7d758fcb
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-05 12:19:38 +1000

    working on regex for hair colors

--- d4p2-solution.py
+++ d4p2-solution.py
@@ -5,7 +5,40 @@ valid = 0
 for lines in data:
 	data_str += lines
 data_str = data_str.split("\n\n")
+valid = 0
 for passport in data_str:
-	if ("byr" in passport) and ("iyr" in passport) and ("eyr" in passport) and ("hgt" in passport) and ("hcl" in passport) and ("ecl" in passport) and ("pid" in passport):
-		valid = valid + 1
+	passport_info = passport.replace("\n"," ")
+	passport_info = passport_info.split(" ")
+	indx_iyr = [passport_info.index(i) for i in passport_info if 'iyr:' in i]
+	indx_byr = [passport_info.index(i) for i in passport_info if 'byr:' in i]
+	indx_eyr = [passport_info.index(i) for i in passport_info if 'eyr:' in i]
+	indx_hgt = [passport_info.index(i) for i in passport_info if 'hgt:' in i]
+	indx_hcl = [passport_info.index(i) for i in passport_info if 'hcl:' in i]
+	indx_ecl = [passport_info.index(i) for i in passport_info if 'ecl:' in i]
+	indx_pid = [passport_info.index(i) for i in passport_info if 'pid:' in i]
+	
+	if indx_iyr == [] or indx_byr ==[] or indx_eyr == [] or indx_hgt == [] or indx_hcl == [] or indx_ecl == [] or indx_pid == []:
+		continue
+	
+	indx_iyr = indx_iyr[0]
+	indx_byr = indx_byr[0]
+	indx_eyr = indx_eyr[0]
+	indx_hgt = indx_hgt[0]
+	indx_hcl = indx_hcl[0]
+	indx_ecl = indx_ecl[0]
+	indx_pid = indx_pid[0]
+	
+	byr = int(passport_info[indx_byr].split(":")[1])
+	iyr = int(passport_info[indx_iyr].split(":")[1])
+	eyr = int(passport_info[indx_eyr].split(":")[1])
+	hgt = passport_info[indx_hgt].split(":")[1]
+	hcl = passport_info[indx_hcl].split(":")[1]
+	ecl = passport_info[indx_ecl].split(":")[1]
+	pid = passport_info[indx_pid].split(":")[1]
+	
+	if (byr >= 1920 and byr <= 2002) and (iyr >= 2010 and iyr <= 2020) and (eyr >= 2020 and eyr <= 2030):
+		valid += 1
+	
+	#	if ("byr" in passport) and ("iyr" in passport) and ("eyr" in passport) and ("hgt" in passport) and ("hcl" in passport) and ("ecl" in passport) and ("pid" in passport):
+#		valid = valid + 1
 print(valid)
