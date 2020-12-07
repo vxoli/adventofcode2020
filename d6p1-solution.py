c81d10c7f70f2089150ad906ca33c6859f3449d9
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-07 19:14:16 +1000

    start d6p1 solution

---
+++ d6p1-solution.py
@@ -0,0 +1,12 @@
+import string
+f = open('d6-input.txt', 'r')
+data = f.readlines()
+
+# clean data string
+print(data)
+# for char in string.ascii_lowercase:
+# loop through a...z
+
+# count occurance of each letter
+
+# calculate total
