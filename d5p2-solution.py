9946621985b46095ef2a8c9a27452107c5a01df8
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-07 16:36:01 +1000

    d5 solutions and output

---
+++ d5p2-solution.py
@@ -0,0 +1,36 @@
+f = open('d5-input.txt', 'r')
+data = f.readlines()
+
+maxseatid=0
+seatid=[]
+
+for idx, line in enumerate(data):
+    row_code = line[:7]
+    seat_code= line[7:]
+    seatrow = []
+    for s in range(0,128,1):
+        seatrow.append(s)
+    for s in row_code:
+        if s == "F":
+            seatrow = seatrow[:int(len(seatrow)/2)]
+        else:
+            seatrow = seatrow[int(len(seatrow)/2):]
+    seatnum = []
+    for s in range(0,8,1):
+        seatnum.append(s)
+    for s in seat_code:
+        if s == "L":
+            seatnum = seatnum[:int(len(seatnum)/2)]
+        else:
+            seatnum = seatnum[int(len(seatnum)/2):]
+    seatid.append(seatrow[0] * 8 + seatnum[0])
+
+seatid.sort()
+for s in range(0,len(seatid)-1):
+    if (seatid[s+1] - seatid[s]) == 1:
+        continue
+    else:
+        print((seatid[s+1]+seatid[s]) / 2)
+
+
+    
