4e4b00027169035301638ae47c38ab11f88a9a3a
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-09 08:57:04 +1000

    d7p1 solution part worked

--- d7p1-solution.py
+++ d7p1-solution.py
@@ -1,2 +1,7 @@
 f = open('d7-input.txt', 'r')
 data = f.readlines()
+
+data_clean = [data[i].split("contains") for i in range(len(data))]
+matching = [data_clean[s] for s in range(len(data_clean)) if "shiny gold" in data_clean[s][1]]
+
+print(len(matching))
