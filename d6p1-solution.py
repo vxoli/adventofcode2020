dbf93ea79bc9b341f16933c166bb93dbe1ed5494
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-08 21:46:01 +1000

    d6p1 solution and output

--- d6p1-solution.py
+++ d6p1-solution.py
@@ -1,12 +1,23 @@
 import string
 f = open('d6-input.txt', 'r')
 data = f.readlines()
+data_clean = []
+lst = ""
+count = 0
+# clean data string - get groups into one line each
+for lines in data:
+	if lines == "\n":
+		data_clean.append(lst.replace("\n", ""))
+		lst = ""
+		continue
+	else:
+		lst += lines
 
-# clean data string
-print(data)
-# for char in string.ascii_lowercase:
+#loop through each line
+for lines in data_clean:
 # loop through a...z
-
-# count occurance of each letter
-
-# calculate total
+	for letter in string.ascii_lowercase:
+# if that letter in the line increment counter
+		if letter in lines:
+			count += 1
+print(count)
