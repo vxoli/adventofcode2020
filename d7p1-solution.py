0e536f153e9537fe9301ed5ee4f46c7204bf099d
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-09 11:36:56 +1000

    d7p1 solution code and output

--- d7p1-solution.py
+++ d7p1-solution.py
@@ -1,7 +1,17 @@
 f = open('d7-input.txt', 'r')
 data = f.readlines()
+# data = ["light red bags contain 1 bright white bag, 2 muted yellow bags.", "dark orange bags contain 3 bright white bags, 4 muted yellow bags.", "bright white bags contain 1 shiny gold bag." , "muted yellow bags contain 2 shiny gold bags, 9 faded blue bags." , "shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags." , "dark olive bags contain 3 faded blue bags, 4 dotted black bags.", "vibrant plum bags contain 5 faded blue bags, 6 dotted black bags." , "faded blue bags contain no other bags.", "dotted black bags contain no other bags."]
+myBags = []
+data_clean = [data[i].split("contain") for i in range(len(data))]
 
-data_clean = [data[i].split("contains") for i in range(len(data))]
-matching = [data_clean[s] for s in range(len(data_clean)) if "shiny gold" in data_clean[s][1]]
+# find the bags that can contain "shiny gold bags"
+myBags = [data_clean[s][0][0:len(data_clean[s][0])-6] for s in range(len(data_clean)) if "shiny gold" in data_clean[s][1]]
 
-print(len(matching))
+# find bags that can contain bags that can contain "shiny gold bags"
+for bag_col in myBags:
+	myBags += [data_clean[s][0][0:len(data_clean[s][0])-6] for s in range(len(data_clean)) if bag_col in data_clean[s][1]]
+
+# remove duplicate bag colours
+myBags = list(dict.fromkeys(myBags))	
+print(myBags)
+print(len(myBags))
