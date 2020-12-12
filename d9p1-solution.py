178bec43cb57d9ece1264def306cef89b126d257
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-12 17:49:40 +1000

    d9p2-solution

--- d9p1-solution.py
+++ d9p1-solution.py
@@ -2,19 +2,23 @@
 f = open('d9-input.txt', 'r')
 data = f.readlines()
 # test data
-data = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
-preamble = 5#25 for main problem, 5 for testing
+# data = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
+preamble = 25 #25 for main problem, 5 for testing
+for i in range(0,len(data)):
+    data[i] = data[i].strip("\n")
+print(data[preamble:])
 
-for i in range(5,len(data)):
+for idx, i in enumerate(data[preamble:]):
     solution = False
-    for j in range(i-5,i):
-        for k in range(i-5,i):
-            if j == k:
-                continue
-            if data[j] + data[k] == data[i]:
-                print(data[j], data[k], data[i])
+    print("idx",idx,"i",i)
+    for j in data[idx:idx+preamble]:
+        for k in data[idx:idx+preamble]:
+            if int(j) + int(k) == int(i) and (int(j) != int(k)):
+                print("i",i,"j",j,"k",k)
+                solution = True
                 break
-            else:
-                print(data[i])
+        if solution == True:
+            break
     if solution == False:
-        print(data[i])
+        print(i)
+        break
