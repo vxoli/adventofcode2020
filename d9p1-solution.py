3207d284b5f8102c3165def51ff2dbf6fca3b22c
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-12 17:51:03 +1000

    d9p1 output

--- d9p1-solution.py
+++ d9p1-solution.py
@@ -6,15 +6,15 @@ data = f.readlines()
 preamble = 25 #25 for main problem, 5 for testing
 for i in range(0,len(data)):
     data[i] = data[i].strip("\n")
-print(data[preamble:])
+# print(data[preamble:])
 
 for idx, i in enumerate(data[preamble:]):
     solution = False
-    print("idx",idx,"i",i)
+    # print("idx",idx,"i",i)
     for j in data[idx:idx+preamble]:
         for k in data[idx:idx+preamble]:
             if int(j) + int(k) == int(i) and (int(j) != int(k)):
-                print("i",i,"j",j,"k",k)
+                # print("i",i,"j",j,"k",k)
                 solution = True
                 break
         if solution == True:
