0df5b126ce0f242a80d0d0e7ebe6c9f57d5a85f6
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-14 19:44:55 +1000

    improved data read lines

--- d10p2-solution.py
+++ d10p2-solution.py
@@ -1,14 +1,10 @@
 # read data file
 f = open('d10-input.txt', 'r')
-data = f.readlines()
+data = [int(line.rstrip()) for line in f.readlines()]
 
 # test data
 # data = [16,10,15,5,1,11,7,19,6,12,4]
 # data = [28,33,18,42,31,14,46,20,48,47,24,23,49,45,19,38,39,11,1,32,25,35,8,17,7,9,4,2,34,10,3]
-
-# convert data to integers
-for i in range(0,len(data)):
-    data[i] = int(data[i].strip("\n"))
 
 # sort data
 data.sort()
