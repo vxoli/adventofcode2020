d6185bb7d583f6cc38e40f272ba313d40cfefa8d
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-16 11:15:20 +1000

    d12p2 solution working on test data

--- d12p2-solution.py
+++ d12p2-solution.py
@@ -1,6 +1,16 @@
+import math
 # read data file
 f = open('d12-input.txt', 'r')
 data = [line.rstrip() for line in f.readlines()]
+
+# routine to rotate waypoint in E-N (x-y) plane
+def rotate(origin, point, angle):
+    # source: https://stackoverflow.com/a/34374437
+    ox, oy = origin
+    px, py = point
+    qx = ox + math.cos(angle) * (px - ox) - math.sin(angle) * (py - oy)
+    qy = oy + math.sin(angle) * (px - ox) + math.cos(angle) * (py - oy)
+    return int(round(qx)), int(round(qy))
 
 #test data
 data = ["F10","N3","F7","R90","F11"]
@@ -10,34 +20,41 @@ data = ["F10","N3","F7","R90","F11"]
 
 directions = {"E","S","W","N"}
 rotations = {"L","R"}
-shipPosn = {"E":0, "S":0, "W":0, "N":0}
-waypoint = {"E":10, "S":0, "W":0, "N":1}
-#dictionary of direction and current value
+
+#dictionary of directions and set initial value
+shipPosn = {"E":0 , "N":0} # W and S indicated by negative values
+waypoint = {"E":10, "N":1} # W and S indicated by negative values
+
 # set starting facing direction to east
 # 90 degrees = E, 180 = S, 270 = W, 0/360 = N
 compass = 90 
 
 for line in data:
-    instruction = line[0]
-    value = int(line[1:])
-    if instruction == "F":
-        if compass == 0:
-            shipPosn["N"] += waypoint["N"] * value
-        if compass == 90:
-            shipPosn["E"] += waypoint["E"] * value
-        if compass == 180:
-            shipPosn["S"] += waypoint["S"] * value
-        if compass == 270:
-            shipPosn["W"] += waypoint["W"] * value
-    if instruction in directions:
-        waypoint[instruction] += value
-    if instruction in rotations:
-        if instruction == "R":
-            compass += value
-            compass = compass % 360
-        if instruction == "L":
-            compass -= value
-            compass = compass % 360
-    
-distance = (shipPosn["E"]-shipPosn["W"]) + (shipPosn["S"]- shipPosn["N"]) 
-print(f"Manhatten distance is: {distance}")
+	instruction = line[0]
+	value = int(line[1:])
+	if instruction == "F":
+		shipPosn["N"] += waypoint["N"] * value
+		shipPosn["E"] += waypoint["E"] * value
+#        if compass == 0:
+#            shipPosn["N"] += waypoint["N"] * value
+#        if compass == 90:
+#            shipPosn["E"] += waypoint["E"] * value
+#        if compass == 180:
+#            shipPosn["S"] += waypoint["S"] * value
+#        if compass == 270:
+#            shipPosn["W"] += waypoint["W"] * value
+	if instruction in directions:
+		if instruction in {"W", "S"}:
+			value = -value
+		waypoint[instruction] += value
+	if instruction in rotations:
+		if instruction == "R":
+			value = - value
+#		compass += value
+#		compass = compass % 360
+		waypoint['E'], waypoint['N'] = rotate((0, 0), (waypoint['E'], waypoint['N']), math.radians(value))
+	
+	print("Ship: E",shipPosn["E"],"N",shipPosn["N"])
+	print("Waypoint: E",waypoint["E"],"N",waypoint["N"])
+#distance = (shipPosn["E"]-shipPosn["W"]) + (shipPosn["S"]- shipPosn["N"]) 
+#print(f"Manhatten distance is: {distance}")
