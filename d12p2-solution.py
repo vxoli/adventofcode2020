b040e962e950abc82efdeb9644083f7af67eb932
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-16 10:37:40 +1000

    added waypoint variable and altered logic for waypoint movements

--- d12p2-solution.py
+++ d12p2-solution.py
@@ -3,14 +3,15 @@ f = open('d12-input.txt', 'r')
 data = [line.rstrip() for line in f.readlines()]
 
 #test data
-# data = ["F10","N3","F7","R90","F11"]
+data = ["F10","N3","F7","R90","F11"]
 
 # ensure input data in list
 # data = [list(line) for line in data]
 
 directions = {"E","S","W","N"}
 rotations = {"L","R"}
-current_posn = {"E":0, "S":0, "W":0, "N":0}
+shipPosn = {"E":0, "S":0, "W":0, "N":0}
+waypoint = {"E":10, "S":0, "W":0, "N":1}
 #dictionary of direction and current value
 # set starting facing direction to east
 # 90 degrees = E, 180 = S, 270 = W, 0/360 = N
@@ -21,15 +22,15 @@ for line in data:
     value = int(line[1:])
     if instruction == "F":
         if compass == 0:
-            current_posn["N"] += value
+            shipPosn["N"] += waypoint["N"] * value
         if compass == 90:
-            current_posn["E"] += value
+            shipPosn["E"] += waypoint["E"] * value
         if compass == 180:
-            current_posn["S"] += value
+            shipPosn["S"] += waypoint["S"] * value
         if compass == 270:
-            current_posn["W"] += value
+            shipPosn["W"] += waypoint["W"] * value
     if instruction in directions:
-        current_posn[instruction] += value
+        waypoint[instruction] += value
     if instruction in rotations:
         if instruction == "R":
             compass += value
@@ -38,5 +39,5 @@ for line in data:
             compass -= value
             compass = compass % 360
     
-distance = (current_posn["E"]-current_posn["W"]) + (current_posn["S"]- current_posn["N"]) 
+distance = (shipPosn["E"]-shipPosn["W"]) + (shipPosn["S"]- shipPosn["N"]) 
 print(f"Manhatten distance is: {distance}")
