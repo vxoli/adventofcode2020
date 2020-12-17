48944e2265f71f5b529abfd6ef1bd9418ede8932
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-17 21:41:36 +1000

    d15p1 if number not spoken before seems to work, number spoken before not working

--- d15p1-solution.py
+++ d15p1-solution.py
@@ -1,21 +1,24 @@
 # given data set of starting numbers
 data = [0,5,4,1,10,14,7]
 
+# test data
+data = [0,3,6]
+
 spokenNumbers = {} #{i:0 for i in data}
 turn = 0
-numberSpoken = 0
+numberSpoken = data[0]
 
 # loop through starting numbers
 while turn < 2020:
     turn += 1
-    spokenNumber = data[turn-1]
+    if turn <= 3:
+        spokenNumber = data[turn-1]
+        print(spokenNumber)
     if spokenNumber in spokenNumbers:
-        spokenNumbers[spokenNumber] = turn
+        spokenNumbers[spokenNumber] = turn - spokenNumbers[spokenNumber]
         data.append(spokenNumber)
-    else:
-        spokenNumbers.update({spokenNumber:0})
-        print(spokenNumbers)
-        spokenNumbers[spokenNumber] = 0
+    if not(spokenNumber in spokenNumbers):
+        spokenNumbers.update({spokenNumber:turn})
         data.append(spokenNumber)
 
 print(data, spokenNumbers)
