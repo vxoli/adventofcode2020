31b2230c71aa1b1467a5140453f4215cf188f0ca
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-17 11:35:54 +1000

    d14p2-solution code and output

--- d14p2-solution.py
+++ d14p2-solution.py
@@ -1,47 +1,50 @@
-# read data file
-f = open('d14-input.txt', 'r')
-data = [line.rstrip() for line in f.readlines()]
-# Test data
-# data = ["mask = XXXXXXXXXXXXXXXXXXXXXXXXXXXXX1XXXX0X","mem[8] = 11","mem[7] = 101", "mem[8] = 0"]
+with open("d14-input.txt", 'r') as fp:
+    lines = [line.rstrip() for line in fp.readlines()]
+masks=[]
+memory = {}
+for line in lines:
+    k, v = line.split(" = ")
+    if k == "mask":
+        masks.append(v)
+        curr_mask = v
+    else:
+        # get address
+        curr_address = int(k[4:-1])
+        curr_value = int(v)       
+        
+        bin_add = list(bin(curr_address)[2:].zfill(36))
+        new_add = ["0"] * 36
+        
+        v = len(list(bin(curr_address)[2:]))
+                
+        for i, (mask, value) in enumerate(zip(curr_mask, bin_add)):
+            # if mask bit is floating then keep it floating on new address too
+            if mask == "X":
+                new_add[i] = "X"
+            elif mask == "0":
+                
+                # change value to mask else
+                new_add[i] = value
+            elif mask == "1":
+                new_add[i] = "1"
+        
+        new_add = "".join(new_add)
+        
+        # count floatings
+        num_poss = new_add.count("X")
+        
+        flucts = []
+        for i in range(2**num_poss):
+            flucts.append( list(bin(i)[2:].zfill(num_poss)))
 
-# define dictionary to store memory addresses
-memoryAddresses = []
-# loop though data and if new memoryadress add to list
-for line in data:
-    if line[:4] == "mem[":
-        #splice the memory address and value from string
-        memAddr = int(line[line.index("[")+1:line.index("]")])
-        if memAddr in memoryAddresses:
-            continue
-        else:
-            memoryAddresses.append(memAddr)
-# use list of memory addresses to define dictionary
-memory = {i:0 for i in memoryAddresses}
-
-for line in data:
-    if line[:4] == "mask":
-        bitmask = line[7:]
-# convert decimal to 36 bit binary: print(format(32, '#038b').replace("0b",""))
-    if line[:4] == "mem[":
-        #splice the memory address and value from string
-        memAddr = int(line[line.index("[")+1:line.index("]")])
-        value = line[line.index("=")+2:]
-        # convert value to 36 bit binary (bin adds 0b to end - once removed 38 length number becomes 36)
-        valueBin = format(int(value), '#038b').replace("0b","")
-        newValueBin = ""
-        # apply bitmask to valueBin to produce newValueBin
-        for idx,digit in enumerate(bitmask):
-            if digit == "X":
-                newValueBin += valueBin[idx]
-            if digit == "1":
-                newValueBin += "1"
-            if digit == "0":
-                newValueBin += "0"
-        # store newValueBin in memory slot memAddr
-        memory[memAddr] = int(newValueBin,2)
-# sum all non-zero value in memory{}
-total = 0
-for i in memory:
-    if memory[i] >0:
-        total += memory[i]
-print(f"sum of values = {total}")
+        for fluct in flucts:
+            i = 0
+            nadd = ""
+            for a in new_add:
+                if a == "X":
+                    nadd+=str(fluct[i])
+                    i+=1
+                else:
+                    nadd+=str(a)
+            memory[int(nadd, 2)]=curr_value
+print(sum(memory.values()))
