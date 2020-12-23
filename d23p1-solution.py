608fddd615a13e18305e14d598c704131117e9bb
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2020-12-23 22:21:03 +1000

    getting closer - recheck the rules - seem to misunderstand them

--- d23p1-solution.py
+++ d23p1-solution.py
@@ -13,7 +13,7 @@ data = [int(i) for i in "389125467"]
 cups = data
 numberCups = len(cups)
 currentCup = cups[0]
-for turn in range (0,5):
+for turn in range (0,10):
     print("Turn: ",turn+1)
     #The crab picks up the three cups that are immediately clockwise of the current cup.
     pickupCups = [cups[(cups.index(currentCup)+1) % numberCups],cups[(cups.index(currentCup)+2) % numberCups],cups[(cups.index(currentCup)+3) % numberCups]]
@@ -32,25 +32,25 @@ for turn in range (0,5):
         destinationCup = ((destinationCup >= min(cups)) * destinationCup) + ((destinationCup < min(cups)) * (max(cups)))
     print("destination cup corrected", destinationCup)
     # The crab places the cups it just picked up so that they are immediately clockwise of the destination cup. They keep the same order as when they were picked up.
+    # store index of destinationCup as will change as string chages
+    destinationCupIndex = cups.index(destinationCup)
     for i in range(len(pickupCups)):
         # delist the elements of pickupCups and add back to cups
         print(i, i+len(pickupCups), pickupCups[i],cups.index(destinationCup)+1+i,cups.index(destinationCup) )
-        if cups.index(destinationCup) == 0:
-            # store index of destinationCup as will change as string chages
-            destinationCupIndex = 0
+        if destinationCupIndex == 0:
             # first element becomes last
-            print(cups)
+            print(cups,cups[0],cups[1:])
             cups.append(cups[0])
+            cups = cups[1:]
             print(cups)
-            cups.pop(0)
-            print(cups)
-            #cup added to front
-            cups.insert(0, pickupCups[i])
+            cups.insert(0,pickupCups[i])
             print(cups)
         else:
             cups.insert(cups.index(destinationCup)+1+i, pickupCups[i])
     # The crab selects a new current cup: the cup which is immediately clockwise of the current cup.
     currentCup = cups[(cups.index(currentCup)+1) % numberCups]
     print(currentCup, cups)
+
+print(cups)
 
 
