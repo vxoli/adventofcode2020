07d4ab7f5d7d771c450c14d4322fb0e965b38d2d
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-14 17:22:48 +1000

    Day 2 in julia starting

---
+++ d02-julia.jl
@@ -0,0 +1,19 @@
+using DelimitedFiles
+
+f = open("d02-input.txt")
+data = readdlm(f, '\t', String, '\n')
+close(f)
+
+# data = [x.strip().split() for x in data]
+# valid = 0
+#for row in range(len(data)):
+	# min value = data[row][0].split('-')[0]
+	# max value = data[row][0].split('-')[1]
+	# character = data[row][1].split(':')[0]
+	# password to test = data[row][2]
+	# number occurances of letter = data[row][2].count(data[row][1].split(':')[0])
+#	max = int(data[row][0].split('-')[1])
+#	min = int(data[row][0].split('-')[0])
+#	char_count = data[row][2].count(data[row][1].split(':')[0])
+#	valid += (char_count >= min) * (char_count <= max)
+#print(valid)
