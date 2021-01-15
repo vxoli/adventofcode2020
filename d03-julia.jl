ae2192cc99450b8cdf3993841c92b19056323c24
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-15 11:51:15 +1000

    d03 julia solution code start

---
+++ d03-julia.jl
@@ -0,0 +1,17 @@
+using DelimitedFiles
+
+f = open("d03-input.txt")
+data = readdlm(f, '\t', String, '\n')
+close(f)
+global trees = 0
+col = Int
+global col = 1
+global line_length = length(data[1])
+for row in 1:length(data)
+    println(row, " ", col)
+#    if data[row][col] == "#"
+#        trees += 1
+#    end
+    col = (col + 3) % (line_length+1)
+end
+print(trees)
