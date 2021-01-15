e069223d0f6addf8c97104ba32f4611e10497a47
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-15 11:27:05 +1000

    julia D02 code solution

--- d02-julia.jl
+++ d02-julia.jl
@@ -17,4 +17,21 @@ for row in 1:length(data)
     char_count = length(collect(eachmatch(Regex(split(split(data[row])[2],":")[1]), split(data[row])[3])))
 	global valid += (char_count >= minimum) * (char_count <= maximum)
 end
-print("Part 1: Valid passwords: ", valid)
+println("Part 1: Valid passwords: ", valid)
+
+valid = 0
+for row in 1:length(data)
+	# char_pos_1 = split(split(data[row])[1],"-")[1]
+	# char_pos_2 = split(split(data[row])[1],"-")[2]
+	# character = split(split(data[row])[2],":")[1]
+	# password to test = split(data[row])[3]
+	# number occurances of letter = length(collect(eachmatch(Regex(split(split(data[row])[2],":")[1]), split(data[row])[3])))
+    test_char = split(split(data[row])[2],":")[1][1]
+    password = split(data[row])[3]
+	char_pos_2 = parse(Int, split(split(data[row])[1],"-")[2])
+    char_pos_1 = parse(Int, split(split(data[row])[1],"-")[1])
+    if (password[char_pos_1] == test_char && password[char_pos_2] != test_char) || (password[char_pos_1] != test_char && password[char_pos_2] == test_char)
+        global valid = valid + 1
+    end
+end
+println("Part 2: Valid passwords: ", valid)
