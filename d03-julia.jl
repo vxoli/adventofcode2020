ded827bbdfaebfcf3c9d311c1da66db03a9c73e1
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-15 12:22:03 +1000

    d03 julia solution code part 1

--- d03-julia.jl
+++ d03-julia.jl
@@ -5,11 +5,11 @@ data = readdlm(f, '\t', String, '\n')
 close(f)
 global trees = 0
 global col = 1
-global line_length = length(data[1])-1
+global line_length = length(data[1])
 for row in 1:length(data)
     if data[row][col] == '#'
         global trees += 1
     end
-    global col = (col + 3) % (line_length)
+    global col = (col + 2) % (line_length)+1 # julia doesnt allow zero index - this calulates across 2 mod line_length then adds one to make total across 3
 end
 print("Trees encountered: $trees")
