3fc8c3065f35795448e55918af518f36df5e0195
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-15 10:37:58 +1000

    part 1 working

--- d02-julia.jl
+++ d02-julia.jl
@@ -5,15 +5,16 @@ data = readdlm(f, '\t', String, '\n')
 close(f)
 
 # data = [x.strip().split() for x in data]
-# valid = 0
-#for row in range(len(data)):
-	# min value = data[row][0].split('-')[0]
-	# max value = data[row][0].split('-')[1]
-	# character = data[row][1].split(':')[0]
-	# password to test = data[row][2]
-	# number occurances of letter = data[row][2].count(data[row][1].split(':')[0])
-#	max = int(data[row][0].split('-')[1])
-#	min = int(data[row][0].split('-')[0])
-#	char_count = data[row][2].count(data[row][1].split(':')[0])
-#	valid += (char_count >= min) * (char_count <= max)
-#print(valid)
+valid = 0
+for row in 1:length(data)
+	# min value = split(split(data[row])[1],"-")[1]
+	# max value = split(split(data[row])[1],"-")[2]
+	# character = split(split(data[row])[2],":")[1]
+	# password to test = split(data[row])[3]
+	# number occurances of letter = length(collect(eachmatch(Regex(split(split(data[row])[2],":")[1]), split(data[row])[3])))
+	maximum = parse(Int, split(split(data[row])[1],"-")[2])
+	minimum = parse(Int, split(split(data[row])[1],"-")[1])
+    char_count = length(collect(eachmatch(Regex(split(split(data[row])[2],":")[1]), split(data[row])[3])))
+	global valid += (char_count >= minimum) * (char_count <= maximum)
+end
+print("Part 1: Valid passwords: ", valid)
