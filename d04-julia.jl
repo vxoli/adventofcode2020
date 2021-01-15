3684f7c11c82d76ef40bca5b0baa17ca08a2155c
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-16 09:37:32 +1000

    d04 part 1 solution complete

--- d04-julia.jl
+++ d04-julia.jl
@@ -1,13 +1,13 @@
-using DelimitedFiles
-
-f = open("d04-input.txt")
-data = readdlm(f, '\n', String, '\n')
+f = open("d04-input.txt", "r")
+data = read(f, String)
 close(f)
+# need to strip the double '\n\n' from the dataset
+data = split(data, "\n\n")
+# Part 1
 valid = 0
 for passport in data
-    println(passport)
 	if (occursin("byr", passport) & occursin("iyr", passport) & occursin("eyr", passport) & occursin("hgt", passport) & occursin("hcl", passport) & occursin("ecl", passport) & occursin("pid", passport))
         global valid = valid + 1
     end
 end
-print(valid)
+print("Part 1: Valid passports = $valid")
