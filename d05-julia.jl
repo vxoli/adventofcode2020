f97b641d9affe596da7b12774afa5c3fd42763a9
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-21 19:31:01 +1000

    d05 part 1 julia code

--- d05-julia.jl
+++ d05-julia.jl
@@ -8,14 +8,12 @@ for line in data
     # split the ticketdata into row and seat info
     global row_code = line[1:7]
     global seat_code= line[8:end]
-    global seatrow = []
     global seatid = 0
-    global seatrow = 1:128
-    global seatnum = []
-    global seatnum = 1:8
+    global seatrow = 0:127
+    global seatnum = 0:7
 
     for s in row_code
-        if s == "F"
+        if s == 'F'
             global seatrow = seatrow[1:Integer(length(seatrow)/2)]
         else
             global seatrow = seatrow[Integer(length(seatrow)/2)+1:end]
@@ -23,7 +21,7 @@ for line in data
     end
 
     for s in seat_code
-        if s == "L"
+        if s == 'L'
             global seatnum = seatnum[1:Integer(length(seatnum)/2)]
         else
             global seatnum = seatnum[Integer(length(seatnum)/2)+1:end]
