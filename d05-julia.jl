0ab1ffdbaea406f0a04cf390608abe88508fe9b0
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-21 17:01:47 +1000

    d05 part 1 julia code

--- d05-julia.jl
+++ d05-julia.jl
@@ -2,37 +2,35 @@ using DelimitedFiles
 f = open("d05-input.txt")
 data = readdlm(f, '\t', String, '\n')
 close(f)
-println(data)
-maxseatid=0
+global maxseatid=0
 
 for line in data
     # split the ticketdata into row and seat info
-    row_code = line[1:7]
-    seat_code= line[8:end]
-    seatrow = []
-    seatid = 0
-    for s in [1:128]
-        seatrow.append(s)
-    end
+    global row_code = line[1:7]
+    global seat_code= line[8:end]
+    global seatrow = []
+    global seatid = 0
+    global seatrow = 1:128
+    global seatnum = []
+    global seatnum = 1:8
+
     for s in row_code
         if s == "F"
-            seatrow = seatrow[1:length(seatrow)/2]
+            global seatrow = seatrow[1:Integer(length(seatrow)/2)]
         else
-            seatrow = seatrow[(length(seatrow)/2)+1:end]
+            global seatrow = seatrow[Integer(length(seatrow)/2)+1:end]
         end
     end
-    seatnum = []
-    for s in range(1,8,1)
-        seatnum.append(s)
-    end
+
     for s in seat_code
         if s == "L"
-            seatnum = seatnum[1:length(seatnum)/2]
+            global seatnum = seatnum[1:Integer(length(seatnum)/2)]
         else
-            seatnum = seatnum[(length(seatnum)/2)+1:end]
+            global seatnum = seatnum[Integer(length(seatnum)/2)+1:end]
         end
+        global seatid = seatrow[1] * 8 + seatnum[1]
+        global maxseatid = (seatid * (seatid > maxseatid)) + (maxseatid * (seatid <= maxseatid))
     end
 end
-    seatid = seatrow[0] * 8 + seatnum[0]
-    maxseatid = (seatid * (seatid > maxseatid)) + (maxseatid * (seatid <= maxseatid))
-    print(seatrow,seatnum, seatid, maxseatid)
+
+println(seatrow[1]," ", seatnum[1], " ", seatid, " ", maxseatid)
