9337f1cfd1b3a03d59700462178a54a6eda78e1f
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-21 19:33:09 +1000

    d05 part 2 julia start

--- d05-julia.jl
+++ d05-julia.jl
@@ -2,8 +2,10 @@ using DelimitedFiles
 f = open("d05-input.txt")
 data = readdlm(f, '\t', String, '\n')
 close(f)
-global maxseatid=0
 
+# Part 1
+
+global maxseatid=0
 for line in data
     # split the ticketdata into row and seat info
     global row_code = line[1:7]
@@ -32,3 +34,36 @@ for line in data
 end
 
 println(seatrow[1]," ", seatnum[1], " ", seatid, " ", maxseatid)
+
+# Part 2
+maxseatid=0
+seatid=[]
+
+for idx, line in enumerate(data):
+    row_code = line[:7]
+    seat_code= line[7:]
+    seatrow = []
+    for s in range(0,128,1):
+        seatrow.append(s)
+    for s in row_code:
+        if s == "F":
+            seatrow = seatrow[:int(len(seatrow)/2)]
+        else:
+            seatrow = seatrow[int(len(seatrow)/2):]
+    seatnum = []
+    for s in range(0,8,1):
+        seatnum.append(s)
+    for s in seat_code:
+        if s == "L":
+            seatnum = seatnum[:int(len(seatnum)/2)]
+        else:
+            seatnum = seatnum[int(len(seatnum)/2):]
+    seatid.append(seatrow[0] * 8 + seatnum[0])
+
+seatid.sort()
+for s in range(0,len(seatid)-1):
+    if (seatid[s+1] - seatid[s]) == 1:
+        continue
+    else:
+        print((seatid[s+1]+seatid[s]) / 2)
+
