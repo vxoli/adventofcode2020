6cd004f7d645d3dc3abf85ecb1007650f24283c3
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-22 09:13:01 +1000

    d05 solution julia code

--- d05-julia.jl
+++ d05-julia.jl
@@ -3,7 +3,7 @@ f = open("d05-input.txt")
 data = readdlm(f, '\t', String, '\n')
 close(f)
 
-# Part 1
+# Part 1 - find highest seat id
 
 global maxseatid=0
 for line in data
@@ -32,38 +32,42 @@ for line in data
         global maxseatid = (seatid * (seatid > maxseatid)) + (maxseatid * (seatid <= maxseatid))
     end
 end
-
-println(seatrow[1]," ", seatnum[1], " ", seatid, " ", maxseatid)
+println("Part 1: Highest seat ID is: $maxseatid")
 
 # Part 2
 maxseatid=0
 seatid=[]
 
-for idx, line in enumerate(data):
-    row_code = line[1:7]
-    seat_code= line[8:end]
-    seatrow = []
-    for s in range(0,128,1):
-        seatrow.append(s)
-    for s in row_code:
-        if s == 'F':
-            seatrow = seatrow[:int(len(seatrow)/2)]
-        else:
-            seatrow = seatrow[int(len(seatrow)/2):]
-    seatnum = []
-    for s in range(0,8,1):
-        seatnum.append(s)
-    for s in seat_code:
-        if s == 'L':
-            seatnum = seatnum[1:Integer(len(seatnum)/2)]
-        else:
-            seatnum = seatnum[Integer(len(seatnum)/2)+1:end]
-    push!(seatid, (seatrow[0] * 8 + seatnum[0]))
+for line in data
+    global row_code = line[1:7]
+    global seat_code= line[8:end]
+    global seatrow = 0:127
+	global seatnum = 0:7
+	
+    for s in row_code
+        if s == 'F'
+            seatrow = seatrow[1:Integer(length(seatrow)/2)]
+        else
+            seatrow = seatrow[Integer(length(seatrow)/2)+1:end]
+		end
+	end
+    for s in seat_code
+        if s == 'L'
+            seatnum = seatnum[1:Integer(length(seatnum)/2)]
+        else
+            seatnum = seatnum[Integer(length(seatnum)/2)+1:end]
+		end
+	end
+    push!(seatid, (seatrow[1] * 8 + seatnum[1]))
+end
 
-seatid.sort()
-for s in range(0,len(seatid)-1):
-    if (seatid[s+1] - seatid[s]) == 1:
+sort!(seatid)
+
+for s in 1:length(seatid)-1
+    if Integer(seatid[s+1] - seatid[s]) == 1
         continue
-    else:
-        print((seatid[s+1]+seatid[s]) / 2)
+    else
+        println("Part 2: seat id is: ", Integer((seatid[s+1]+seatid[s]) / 2))
+	end
+end
 
