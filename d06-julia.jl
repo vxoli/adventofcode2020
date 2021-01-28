3eb02bc08834549ce120073455ba46596f05535f
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-28 17:25:17 +1000

    d06 code - over counting

--- d06-julia.jl
+++ d06-julia.jl
@@ -1,28 +1,13 @@
-using DelimitedFiles
-f = open("d06-input.txt", "r")
-data = readdlm(f,'\t', String, '\n')
-close(f)
+readInput() = split.(split(read(joinpath(@__DIR__, "d06-input.txt"), String), "\n\n"))
+data = readInput()
 
-global data_clean = []
-global list = ""
 global count = 0
 
-# clean data string - get groups into one line each
-#for lines in data
-#	if lines == '\n'
-#		global data_clean = data_clean * replace(list, "\n" => "")
-#		global list = ""
-#		continue
-#	else
-#        global list = list * lines
-#    end
-#end
-# loop through each line
 for lines in data
-# loop through a...z
+    # loop through a...z
     for letter in collect('a':'z')
-# if that letter in the line increment counter
-		if letter in lines
+        # if that letter in the line increment counter
+		if occursin(letter, string(lines))
             global count += 1
         end
     end
