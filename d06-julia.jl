5f4788fcd7bb28fc41cbb9c22e1dd5f7b61ed4da
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-29 18:16:33 +1000

    d06 julia code

--- d06-julia.jl
+++ d06-julia.jl
@@ -1,18 +1,22 @@
 readInput() = split.(split(read("d06-input.txt", String), "\n\n"))
 data = readInput()
 
-# Part 1
+# solve by creating a set of responses.
+# Part 1 : answers for anyone is the union of sets of answers
+# Part 2 : answers for everyone in the intersection of the sets of answers
 
 global part1 = 0
 global part2 = 0
 for line in data
-	txt = ""
+    if line == [] continue end
+    txt1 = ""
+    txt2 = line[1]
 	for x in 1:length(line)
-		txt = ∪(txt,line[x]) # ∪ symbol for union
+        txt1 = ∪(txt1,line[x]) # ∪ symbol for union
+        txt2 = ∩(txt2, line[x]) # ∩ symbol for intersection
 	end
-	global part1 += length(txt)
+    global part1 += length(txt1)
+    global part2 += length(txt2)
 end
 println("Part 1: the number of questions to which anyone answered \"yes\" = : $part1")
-
-# Part 2
-
+println("Part 2: the number of questions to which everyone answered \"yes\" = : $part2")
