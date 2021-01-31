105d8bb4b322da12e79129251c321fe53be47e8f
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-31 15:20:26 +1000

    d08 part 1 julia complete

--- d08-julia.jl
+++ d08-julia.jl
@@ -1,33 +1,38 @@
 # read data file
-f = open('d08-input.txt', 'r')
-data = f.readlines()
+readInput() = split(read("d08-input.txt", String), "\n")
+data = readInput()
 # test data
 # data = ["nop +0", "acc +1", "jmp +4", "acc +3", "jmp -3", "acc -99", "acc +1", "jmp -4", "acc +6"]
 
 # initialise variables
-codeLine = 0
-accumulator = 0
-executedCodeLines = []
-repeatedLines = False
+global codeLine = 1
+global accumulator = 0
+global executedCodeLines = []
+global repeatedLines = false
 
 # loop until a line is repeated
-while not(repeatedLines):
+while !(repeatedLines)
     # split input data into components
-    command = data[codeLine][0:3]
-    value = data[codeLine][3:]
+    command = data[codeLine][1:3]
+    value = parse(Int64, data[codeLine][5:end])
     # if the line of code has been executed then change the repeatedLines variable
-    if codeLine in executedCodeLines:
-        repeatedLines = True
+    if codeLine in executedCodeLines
+        global repeatedLines = true
         break
-    else: executedCodeLines.append(codeLine)
+    else push!(executedCodeLines, codeLine)
+    end
     # executed the commands
-    if command == "acc":
-        accumulator += int(value)
+    if command == "acc"
+        global accumulator += value
+        global codeLine += 1
+    end
+    if command == "nop"
         codeLine += 1
-    if command == "nop":
-        codeLine += 1
-    if command == "jmp":
-        codeLine += int(value)
-
+    end
+    if command == "jmp"
+        codeLine += value
+    end
+end
 # print result
-print(accumulator)
+println("Immediately before any instruction is executed a second time, what value is in the accumulator?")
+println(accumulator)
