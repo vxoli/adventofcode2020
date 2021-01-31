de16d5e739a208caca1452539e86037cf5daad33
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-01-31 17:21:52 +1000

    FizzBuzz - just for fun!

---
+++ FizzBuzz.jl
@@ -0,0 +1,23 @@
+#FizzBuzz
+start = 0
+finish = 100
+
+for count = start:finish
+    FizzBuzzString = ""
+    if (count % 3 == 0) & (count % 5 == 0)
+        FizzBuzzString = "FizzBuzz"
+    elseif (count % 3 == 0) & (count % 5 != 0)
+            FizzBuzzString = "Fizz"
+    elseif (count % 3 != 0) & (count % 5 == 0)
+            FizzBuzzString = "Buzz"
+    else
+        FizzBuzzString = string(count)
+    end
+    println(FizzBuzzString)
+end
+
+# Rockstart version
+# z(i)=(f=i .%[3,5] .==0;sum(f)>0 ? foldl(*,["Fizz","Buzz"][f]) : i)
+# println.(z.(1:100))
+
+    
