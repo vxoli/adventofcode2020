e49d42b602c110bef8b71cb0a11acb78f6f3bb9e
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-02-05 15:30:16 +1000

    starting d09 julia code

--- d09-julia.jl
+++ d09-julia.jl
@@ -1,18 +1,24 @@
 # read data file
 readInput() = split(read("d09-input.txt", String), "\n")
-data = readInput()
+stringData = readInput()
+# convert strings to Integer
+data = []
+for i in stringData
+    push!(data, parse(Int,i))
+end
+
 # test data
 # data = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
 preamble = 25 #25 for main problem, 5 for testing
 
-for (idx, i) in enumerate(data[preamble:end])
-    solution = false
+for i in data[preamble:end]
+    global solution = false
     # print("idx",idx,"i",i)
-    for j in data[idx:idx+preamble]
-        for k in data[idx:idx+preamble]
-            if (j + k == i) &  (j != k)
-                # print("i",i,"j",j,"k",k)
-                solution = true
+    for j in data[i:i+preamble]
+        for k in data[i:i+preamble]
+            if (j + k == i) &&  (j != k)
+                # println("i",i,"j",j,"k",k)
+                global solution = true
                 break
             end
         end
