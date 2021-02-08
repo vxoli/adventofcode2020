ff4a9788d7067aded974c4c7553d0dd3edc57c9a
Christopher Stonell <vxoli-dfoxb@yahoo.co.uk>
2021-02-08 16:06:21 +1000

    d08 p1 julia wokring

--- d09-julia.jl
+++ d09-julia.jl
@@ -11,11 +11,11 @@ end
 # data = [35, 20, 15, 25, 47, 40, 62, 55, 65, 95, 102, 117, 150, 182, 127, 219, 299, 277, 309, 576]
 preamble = 25 #25 for main problem, 5 for testing
 
-for i in data[preamble:end]
+for (idx,i) in enumerate(data[preamble+1:end])
     global solution = false
-    # print("idx",idx,"i",i)
-    for j in data[i:i+preamble]
-        for k in data[i:i+preamble]
+    # println("idx",idx," i",i)
+    for j in data[idx:idx+preamble-1]
+        for k in data[idx:idx+preamble-1]
             if (j + k == i) &&  (j != k)
                 # println("i",i,"j",j,"k",k)
                 global solution = true
