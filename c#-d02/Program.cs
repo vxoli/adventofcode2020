6cec0d3c2e6c234de97fc5d3034ac8cc5ae0c70d
Christopher Stonell <chris_stonell@yahoo.co.uk>
2021-05-06 12:09:34 +1000

    d02 c# solution start

---
+++ c#-d02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__d02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
