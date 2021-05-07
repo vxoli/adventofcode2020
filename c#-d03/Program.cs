02c0ed88ceb8815cbe17cd09233896c843d5d6aa
Christopher Stonell <chris_stonell@yahoo.co.uk>
2021-05-07 11:37:21 +1000

    c# d03 part 1 solution complete

--- c#-d03/Program.cs
+++ c#-d03/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
-
+using System.IO;
 namespace c__d03
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] map = File.ReadAllLines("../d03-input.txt");
+            Int16 trees = 0;
+            int col = 0;
+            int rowsTotal = map.GetLength(0);
+            var length = map[0].Length;
+            for (int row = 0; row < rowsTotal; row++)
+            {
+                if (String.Equals(map[row][col], "#"[0])) trees++;
+                col = (col+3) % (length);
+            }
+            Console.WriteLine("Part 1:");
+            Console.WriteLine("How many trees would you encounter? {0}", trees);
+
         }
     }
 }
+//data = f.readlines()
+//trees = 0
+//col = 0
+//length = len(data[0])
+//for row in range(len(data)):
+//    if data[row][col] == "#":
+//            trees += 1 
+//    col = (col + 3) % (length-1)
+//print(trees)
