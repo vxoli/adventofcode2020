3e59c59cfc4692356b541318c5d1ddde7a04ece3
Christopher Stonell <chris_stonell@yahoo.co.uk>
2021-05-11 16:08:13 +1000

    c# d04 solution complete

--- c#-d04/Program.cs
+++ c#-d04/Program.cs
@@ -23,6 +23,7 @@ namespace c__d04
             Console.WriteLine("In your batch file, how many passports are valid? {0}", valid);
 
         // Part 2 Solution
+            valid = 0;
             foreach (string passport in passports)
             {
                 string passportInfo = passport.Replace("\n", " ");
@@ -60,15 +61,15 @@ namespace c__d04
                 // setup Regex pattersn for hcl and pid
                 string pattern = @"^#[0-9A-Fa-f]{6}$";
                 Regex hclRegex = new Regex(pattern);
-                pattern = @"\d{9}";
-                Regex pidRegex = new Regex(pattern);
+                bool hcl_valid = hclRegex.IsMatch(hcl);
 
-                //bool hcl_valid = hclRegex.Matches(hcl);
-
-                //va;lidity test for eye colour
+                //validity test for eye colour
                 bool ecl_valid = ecl.Equals("amb") | ecl.Equals("blu") | ecl.Equals("brn") | ecl.Equals("gry") | ecl.Equals("grn") | ecl.Equals("hzl") | ecl.Equals("oth");
 
-                //bool pid_valid = pidrEGEX.Matches(pid);
+                //validity test for passport ID
+                pattern = @"\d{9}";
+                Regex pidRegex = new Regex(pattern);
+                bool pid_valid = pidRegex.IsMatch(pid);
                 
                 // validity tests for height
                 //1 st: seperate units from value
@@ -81,46 +82,15 @@ namespace c__d04
                 string hgt_units = hgt.Substring(hgt.Length-2,2);
                 bool hgt_valid = (hgt_value >= 150 & hgt_value <= 192 & hgt_units.Equals("cm")) | (hgt_value >= 59 & hgt_value <= 76 & hgt_units.Equals("in"));
 
+                //if all the etsts are valid then the passport is counted as a valid passport
+                if (byr_valid & iyr_valid & eyr_valid & hcl_valid & ecl_valid & pid_valid & hgt_valid) valid++;
 
-                //if ((byr >= 1920 & byr <= 2002) & (iyr >= 2010 & iyr <= 2020) & (eyr >= 2020 & eyr <= 2030) & (hclRegex.Matches(hcl)) & (ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]) & (pidRegex.Matches(pid)) & ( ((hgt.EndsWith("cm") & int(hgt[:len(hgt)-2]) >= 150 & int(hgt[:len(hgt)-2]) <= 192)) | ((hgt.EndsWith("in") & int(hgt[:len(hgt)-2]) >= 59 & int(hgt[:len(hgt)-2]) <= 76)) ):
 
 
             }
+            Console.WriteLine("Part 2:");
+            Console.WriteLine("In your batch file, how many passports are valid? {0}", valid);
 
         }
     }
 }
-//valid = 0
-//for passport in data_str:
-//	passport_info = passport.replace("\n"," ")
-//	passport_info = passport_info.split(" ")
-//	indx_iyr = [passport_info.index(i) for i in passport_info if 'iyr:' in i]
-//	indx_byr = [passport_info.index(i) for i in passport_info if 'byr:' in i]
-//	indx_eyr = [passport_info.index(i) for i in passport_info if 'eyr:' in i]
-//	indx_hgt = [passport_info.index(i) for i in passport_info if 'hgt:' in i]
-//	indx_hcl = [passport_info.index(i) for i in passport_info if 'hcl:' in i]
-//	indx_ecl = [passport_info.index(i) for i in passport_info if 'ecl:' in i]
-//	indx_pid = [passport_info.index(i) for i in passport_info if 'pid:' in i]
-//	
-//	if indx_iyr == [] or indx_byr ==[] or indx_eyr == [] or indx_hgt == [] or indx_hcl == [] or indx_ecl == [] or indx_pid == []:
-//		continue
-//	
-//	indx_iyr = indx_iyr[0]
-//	indx_byr = indx_byr[0]
-//	indx_eyr = indx_eyr[0]
-//	indx_hgt = indx_hgt[0]
-//	indx_hcl = indx_hcl[0]
-//	indx_ecl = indx_ecl[0]
-//	indx_pid = indx_pid[0]
-//	
-//	byr = int(passport_info[indx_byr].split(":")[1])
-//	iyr = int(passport_info[indx_iyr].split(":")[1])
-//	eyr = int(passport_info[indx_eyr].split(":")[1])
-//	hgt = passport_info[indx_hgt].split(":")[1]
-//	hcl = passport_info[indx_hcl].split(":")[1]
-//	ecl = passport_info[indx_ecl].split(":")[1]
-//	pid = passport_info[indx_pid].split(":")[1]
-//	
-//	if (byr >= 1920 and byr <= 2002) and (iyr >= 2010 and iyr <= 2020) and (eyr >= 2020 and eyr <= 2030) and (re.findall("^#[0-9A-Fa-f]{6}$", hcl)) and (ecl in ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]) and (re.findall('\d{9}', pid)) and ( ((hgt.endswith("cm") and int(hgt[:len(hgt)-2]) >= 150 and int(hgt[:len(hgt)-2]) <= 192)) or ((hgt.endswith("in") and int(hgt[:len(hgt)-2]) >= 59 and int(hgt[:len(hgt)-2]) <= 76)) ):
-//		valid += 1
-//print(valid)
