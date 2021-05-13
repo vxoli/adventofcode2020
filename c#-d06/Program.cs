2de140b580a5d9d45e81185c4d28232e5e4722e8
Christopher Stonell <chris_stonell@yahoo.co.uk>
2021-05-13 16:02:08 +1000

    c# d06 part 2 start

--- c#-d06/Program.cs
+++ c#-d06/Program.cs
@@ -29,30 +29,38 @@ namespace c__d06
             }
             Console.WriteLine("Part 1");
             Console.WriteLine("For each group, count the number of questions to which anyone answered \"yes\". What is the sum of those counts? :{0}",count);
+            Console.WriteLine("Part 2:");
+            Console.WriteLine("For each group, count the number of questions to which everyone answered \"yes\". What is the sum of those counts? :{0}",count);            
         }
     }
 }
 
-//import string
-//f = open('d06-input.txt', 'r')
-//data = f.readlines()
-//data_clean = []
-//lst = ""
-//count = 0
-//# clean data string - get groups into one line each
-//for lines in data:
-//	if lines == "\n":
-//		data_clean.append(lst.replace("\n", ""))
-//		lst = ""
-//		continue
-//	else:
-//		lst += lines
-//
-//#loop through each line
-//for lines in data_clean:
-//# loop through a...z
+//for i in data_clean:
 //	for letter in string.ascii_lowercase:
-//# if that letter in the line increment counter
-//		if letter in lines:
+//		letter_count = 0
+//		for j in range(0,len(i)-1):
+//			letter_count += i[j].count(letter)
+//			print(lines, i,i[j],letter,letter_count, count)
+//		if letter_count == len(i)-1:
 //			count += 1
+
+		
+//#		for i in range(0,len(group)-1):
+//#			for idx, letter in enumerate(string.ascii_lowercase):
+//#				print(idx, letter, i, group[i])
+//#				if letter in group[i]:
+//#					lttrs[idx] += 1
+//#					print(lttrs[idx])
+//#				if letters[idx] == len(group)-1:
+//#					count += 1
+//#		group = []
+//#		lttrs = []
 //print(count)
+//# clean data string - get groups into one line each
+//#for lines in data:
+//#	if lines == "\n":
+//#		data_clean.append(lst.replace("\n", ""))
+//#		lst = ""
+//#		continue
+//#	else:
+//#		lst += lines
