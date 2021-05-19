4e64e4d29eed254c9f925cd9a81bcee19751a969
Christopher Stonell <chris_stonell@yahoo.co.uk>
2021-05-20 09:17:10 +1000

    c# d09 setup

---
+++ c#-d09/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c__d09
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
