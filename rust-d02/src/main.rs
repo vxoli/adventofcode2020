0d10db36c2f08bd28af01ea158bcfeb293656d61
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-07-26 14:10:11 +1000

    rust D02 complete

--- rust-d02/src/main.rs
+++ rust-d02/src/main.rs
@@ -1,5 +1,6 @@
 /* # Day 2: 
 ## --- Password Philosophy ---
+## --- Part One ---
 
 Your flight departs in a few days from the coastal airport; the easiest way down to the coast from here is via toboggan.
 
@@ -21,26 +22,52 @@ In the above example, 2 passwords are valid. The middle password, cdefg, is not;
 
 How many passwords are valid according to their policies?
 
-To begin, get your puzzle input. */
+To begin, get your puzzle input. 
+
+## --- Part Two ---
+
+While it appears you validated the passwords correctly, they don't seem to be what the Official Toboggan Corporate Authentication System is expecting.
+
+The shopkeeper suddenly realizes that he just accidentally explained the password policy rules from his old job at the sled rental place down the street! The Official Toboggan Corporate Policy actually works a little differently.
+
+Each policy actually describes two positions in the password, where 1 means the first character, 2 means the second character, and so on. (Be careful; Toboggan Corporate Policies have no concept of "index zero"!) Exactly one of these positions must contain the given letter. Other occurrences of the letter are irrelevant for the purposes of policy enforcement.
+
+Given the same example list from above:
+
+- 1-3 a: abcde is valid: position 1 contains a and position 3 does not.
+- 1-3 b: cdefg is invalid: neither position 1 nor position 3 contains b.
+- 2-9 c: ccccccccc is invalid: both position 2 and position 9 contain c.
+
+How many passwords are valid according to the new interpretation of the policies?
+*/
 
 use std::fs;
 
 fn main() {
     let filename = "/home/christopher/Documents/GitHub/adventofcode2020/d02-input.txt";
     let data = read_input_data(filename);
-    let mut valid: i32 = 0;
+    let mut validpart1: i32 = 0;
+    let mut validpart2: i32 = 0;
     for (_index, line) in data.iter().enumerate() {
         // split the string into min, max, letter and pasword
         let (min, max, letter, password) = split_the_line(line);
         // count occurrences of the letter in the password
         let occurrences = count_occurrences(letter, password);
-        // check if count falls into the rage allowed
-        if occurrences <= max && occurrences >= min {
-            valid += 1;
+        // Part 1: check if count falls into the rage allowed
+        if occurrences <= max && occurrences >= min {validpart1 += 1; }
+        // Part 2 check if one and only one of the positions in password contains the letter
+        let part2 = password.chars();
+        let mut valid: bool = false;
+        for (index, ch) in part2.enumerate() {
+            if ((index + 1) as i32 == min || (index+1) as i32 == max )&& ch == letter {valid = !valid;}
         }
+        if valid {validpart2 += 1;}
     }
-    println!("Part 1: valid password = {}", valid);
-}
+    println!("Part 1: valid passwords = {}", validpart1);
+    println!("Part 2: valid passwords = {}", validpart2);
+} // End of main function
+
+//Start function definitions
 
 fn read_input_data(filename: &str) -> Vec<String> {
     let input = fs::read_to_string(filename)
