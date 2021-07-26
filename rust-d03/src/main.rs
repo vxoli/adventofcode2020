12f8b47702a11b35593ba2a4e0b1ba8fd1788088
Christopher Stonell laptop <chris_stonell@yahoo.co.uk>
2021-07-26 17:56:12 +1000

    rust d03 complete

--- rust-d03/src/main.rs
+++ rust-d03/src/main.rs
@@ -57,20 +57,36 @@ The locations you'd check in the above example are marked here with O where ther
 In this example, traversing the map using this slope would cause you to encounter 7 trees.
 
 Starting at the top-left corner of your map and following a slope of right 3 and down 1, how many trees would you encounter?
- */
+
+## --- Part Two ---
+
+Time to check the rest of the slopes - you need to minimize the probability of a sudden arboreal stop, after all.
+
+Determine the number of trees you would encounter if, for each of the following slopes, you start at the top-left corner and traverse the map all the way to the bottom:
+
+-    Right 1, down 1.
+-    Right 3, down 1. (This is the slope you already checked.)
+-    Right 5, down 1.
+-    Right 7, down 1.
+-    Right 1, down 2.
+
+In the above example, these slopes would find 2, 7, 3, 4, and 2 tree(s) respectively; multiplied together, these produce the answer 336.
+
+What do you get if you multiply together the number of trees encountered on each of the listed slopes?
+*/
 
 fn main() {
     let data = read_input_data("/home/christopher/Documents/GitHub/adventofcode2020/d03-input.txt");
-    let mut col: i32 = 0;
-    let line_length: i32 = (data[0].len() as i32) -1;
-    let mut trees:i32 = 0;
-    for row in data.iter() {
-        if row.chars().nth(col as usize).unwrap() == '#' {trees += 1;}
-        col += 3;
-        col = col % (line_length + 1);
-
-    }
+    // Part 1
+    let mut trees: i32 = toboggan_tragectory(&data, 1, 3);
     println!("Part 1: trees encounted = {}", trees);
+    // Part 2
+    trees *= toboggan_tragectory(&data,1,1);
+    // trees = toboggan_tragectory(&data,3,1); // already soled this in part 1 - dont need to repeat
+    trees *= toboggan_tragectory(&data,1,5);
+    trees *= toboggan_tragectory(&data,1,7);
+    trees *= toboggan_tragectory(&data,2,1);
+    println!("Part 2: trees encounted = {}", trees);
 }
 
 //Start function definitions
@@ -82,4 +98,21 @@ fn read_input_data(filename: &str) -> Vec<String> {
                     .map(|line| line.to_string())
                     .collect();
     input
+}
+
+fn toboggan_tragectory(map_data: &Vec<String>, delta_row: i32, delta_col: i32) -> i32 {
+    let mut col: i32 = 0;
+    let mut row: i32 = 0;
+    let rows: i32 = map_data.len() as i32;
+    let line_length: i32 = (map_data[0].len() as i32) -1;
+    let mut trees:i32 = 0;
+    loop {
+        let map_row = &map_data[row as usize];
+        if map_row.chars().nth(col as usize).unwrap() == '#' {trees += 1;}
+        col += delta_col;
+        col = col % (line_length + 1);
+        row += delta_row;
+        if row >= rows {break;}
+    }
+    trees
 }
